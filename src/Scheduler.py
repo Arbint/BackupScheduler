baa54b6a12f33e8e8e4201745de7cb66d3412a1a
Jingtian Li <jingtianli@jingtians-air.attlocal.net>
2024-12-28 01:40:18 -0600

    Added Basic Classes: * Gui class done * Scheduler class have values updated through the ui * scheduler class need to implement the actually peoridical backup   routine

---
+++ src/Scheduler.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import shutil
+
+class Scheduler:
+    def __init__(self):
+        self.folderToBackup = ""
+        self.backupDestination = ""
+        self.backupIntervalMinutes = 5 
+
+    def SetBackupIntervalMintues(self, newMinutes: int):
+        self.backupIntervalMinutes = newMinutes
+
+    def SetFolderToBackup(self, newFolderToBackup: str):
+        self.folderToBackup = newFolderToBackup
+
+    def SetBackupDestination(self, newBackupDestination: str):
+        self.backupDestination = newBackupDestination
+
+    def StartBackupRoutine(self):
+        print(f"starting backup {self.folderToBackup} to {self.backupDestination}, with interval: {self.backupIntervalMinutes} minutes")
---
+++ src/SchedulerGUI.py
@@ -0,0 +1,60 @@
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QFileDialog, QPushButton, QLabel, QGridLayout, QApplication, QLineEdit
+from PySide6.QtGui import QIntValidator 
+from Scheduler import Scheduler 
+
+class ScheduleGUI(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.scheduler = Scheduler()
+
+        self.setWindowTitle("Backup Scheduler")
+        self.masterLayout = QVBoxLayout()
+        self.setLayout(self.masterLayout)
+
+        self.BuildFolderQuerySection("Folder to Backup: ", self.scheduler.SetFolderToBackup)
+        self.BuildFolderQuerySection("Backup Destionation: ", self.scheduler.SetBackupDestination)
+
+        backupIntervalLabel = QLabel("Backup Interval Minutes")
+        self.masterLayout.addWidget(backupIntervalLabel)
+        
+        self.backupIntervalLineEdit = QLineEdit()
+        self.backupIntervalLineEdit.setValidator(QIntValidator(bottom=1))
+        self.backupIntervalLineEdit.textChanged.connect(self.BackupIntervalChanged)
+        self.masterLayout.addWidget(self.backupIntervalLineEdit)
+
+        self.startBackupRoutineBtn = QPushButton("Start Backup Routine")
+        self.startBackupRoutineBtn.clicked.connect(self.scheduler.StartBackupRoutine)
+        self.masterLayout.addWidget(self.startBackupRoutineBtn)
+
+    def BackupIntervalChanged(self):
+        intervalStr = self.backupIntervalLineEdit.text()
+        self.scheduler.SetBackupIntervalMintues(int(intervalStr))
+
+    def BuildFolderQuerySection(self, label: str, ConfigFunc):
+        sectionLayout = QHBoxLayout()
+        self.masterLayout.addLayout(sectionLayout)
+
+        folderLabel = QLabel(label)
+        sectionLayout.addWidget(folderLabel)
+
+        folderLineEdit = QLineEdit()
+        folderLineEdit.setEnabled(False)
+        sectionLayout.addWidget(folderLineEdit)
+
+        selectFolderBtn = QPushButton("...")
+        selectFolderBtn.clicked.connect(lambda : self.SelectFolder(folderLineEdit, ConfigFunc))
+        sectionLayout.addWidget(selectFolderBtn)
+
+    def SelectFolder(self, folderLineEdit: QLineEdit, DirConfigFunc):
+        dir = QFileDialog().getExistingDirectory()
+        DirConfigFunc(dir)
+        folderLineEdit.setText(dir)
+
+if __name__ == "__main__":
+    app = QApplication()
+
+    gui = ScheduleGUI()
+    gui.show()
+
+    app.exec()
