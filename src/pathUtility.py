71aafe4f40c02071792185abb4eff43bd1aaed72
Arbint <cg.jingtianli@gmail.com>
2025-01-08 18:23:42 -0600

    Trying to make a snapshot mechanism

--- src/pathUtility.py
+++ src/pathUtility.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import os
+import Logger
+import subprocess
 
 def GetSrcDir():
     return Path(__file__).resolve().parent
@@ -19,3 +21,30 @@ def GetRecordFilePath():
 
     return dataPath.resolve()
 
+def CreateSnapshotWindows(dirToSnapShot):
+    diskshadowScriptPath = os.path.join(GetSrcDir(), "diskshadowScript.txt")
+    try:
+        snapshotDrive = "X:"
+        diskShadowScript = f"""
+        set context persistent
+        add volume {os.path.splitdirve(dirToSnapShot)[0]} alias "snapshotAlias"
+        create
+        expose %snapshotAlias% {snapshotDrive}
+        """
+
+        with open(diskshadowScriptPath, "w") as scriptFile:
+            scriptFile.write(diskShadowScript)
+
+        subprocess.run(["diskshadow", "/s", diskshadowScriptPath], check=True)
+        os.remove(diskshadowScriptPath)
+        return snapshotDrive + dirToSnapShot[len(os.path.splitdrive(dirToSnapShot)[0]):]
+
+    except Exception as e:
+        if os.path.exists(diskshadowScriptPath):
+            os.remove(diskshadowScriptPath)
+
+        Logger.Logger.AddLogEntry(f"Failed to create windows snapshot {e}", True)
+        return None
+
+
+print(CreateSnapshotWindows(GetPrjDir(), ))
