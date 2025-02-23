a4283c5256c28cfad5fc92cdd8aba3bc7216371d
Prof Li <jili1@uiwtx.edu>
2025-02-23 19:42:04 +0000

    trying to implement the linux backup routine

--- src/P4Backup.py
+++ src/P4Backup.py
@@ -6,19 +6,18 @@ import Logger
 import re
 from Communcation import Emailer
 
+
 class P4Backup(Backup):
     def __init__(self):
         self.p4ServiceName = "perforce"
-        self.windowsStartServiceCmd = ["sc", "start", self.p4ServiceName]
-        self.windowsStopServiceCmd = ["sc", "stop", self.p4ServiceName]
-        self.windowsQueryServiceCmd = ["sc", "query", self.p4ServiceName]
-        self.runningState = "RUNNING"
-        self.stoppedState = "STOPPED"
-        self.startPendingState = "START_PENDING"
-        self.stopPendingState = "STOP_PENDING"
         self.emailer = Emailer()
         self.shouldInfomUsers = False
-        
+
+    def PreBackup(self):
+        pass
+
+    def PostBackup(self):
+        pass
 
     def DoBackupImpl(self, p4ServerRoot: str, backupDestination: str):
         """
@@ -28,7 +27,7 @@ class P4Backup(Backup):
         """
 
         try:
-            self.StopServer()
+            self.PreBackup()
             os.makedirs(backupDestination, exist_ok = True)
 
             # backup checkpoint and journal
@@ -50,9 +49,10 @@ class P4Backup(Backup):
             if os.path.exists(configFileSrcPath):
                 self.BackupComponent(configFileSrcPath, configFileDestPath)
             
-            self.RestartServer()
+            self.PostBackup()
+
         except Exception as e:
-            self.RestartServer()
+            self.PostBackup()
             print(f"can't backup server: {e}")
 
     def CreateCheckpointAndRotateJournal(self, p4ServerRoot):
@@ -94,23 +94,6 @@ class P4Backup(Backup):
         except Exception as e:
             print(f"Can't backup depot {e}")
 
-    def RestartServer(self):
-        print(f"####### Trying To Restart Server ############")
-        if self.IsServerPendingStart() or self.IsServerRunning():
-            print(f"Server Started or pending Start, not need to start again")
-            return
-        subprocess.run(self.windowsStartServiceCmd, check=True)   
-        self.SendEmailToAllUsers("Perforce Server Is Back Online", self.GetServerBackOnlineModeMsg())
-
-
-    def StopServer(self):
-        print(f"####### Trying To Stop Server ############")
-        if self.IsServerPendingStop() or self.IsServerStopped():
-            print(f"Server stopped or pending stop, no need to stop again")
-            return 
-        subprocess.run(self.windowsStopServiceCmd, check=True)
-        self.SendEmailToAllUsers("Perforce Server In Backup Mode", self.GetServerBackOnlineModeMsg())
-
 
     def BackupComponent(self, src, destDir):
         try:
@@ -125,7 +108,7 @@ class P4Backup(Backup):
 
     def BackupTerminated(self):
         Logger.Logger.AddLogEntry(f"p4 backup process terminated, re institiating server")
-        self.RestartServer()
+        self.PostBackup()
 
     def IsServerRunning(self):
         return self.GetServeryStat() == self.runningState
@@ -139,22 +122,6 @@ class P4Backup(Backup):
     def IsServerPendingStop(self):
         return self.GetServeryStat() == self.stopPendingState
 
-    def GetServeryStat(self):
-        try: 
-            result = subprocess.run(self.windowsQueryServiceCmd, capture_output=True, text=True)
-            print(f"---------------quried server stat: {result.stdout} \n -------------")
-            if self.runningState in result.stdout:
-                return self.runningState
-            
-            if self.startPendingState in result.stdout:
-                return self.startPendingState
-
-            if self.stopPendingState in result.stdout:
-                return self.stopPendingState
-
-        except subprocess.CalledProcessError as e:
-            print(f"Error checking server status: {e}")
-            return None
         
     def GetAllUserEmails(self):
         users = subprocess.run("p4 users", capture_output=True, text=True)        
@@ -174,3 +141,67 @@ class P4Backup(Backup):
 
     def GetServerBackOnlineModeMsg(self):
         return """This an aotomated email, the perfore server is backup online."""
+
+
+class P4BackupWindows(P4Backup):
+    def __init__(self):
+        super().__init__()
+        self.windowsStartServiceCmd = ["sc", "start", self.p4ServiceName]
+        self.windowsStopServiceCmd = ["sc", "stop", self.p4ServiceName]
+        self.windowsQueryServiceCmd = ["sc", "query", self.p4ServiceName]
+        self.runningState = "RUNNING"
+        self.stoppedState = "STOPPED"
+        self.startPendingState = "START_PENDING"
+        self.stopPendingState = "STOP_PENDING"
+
+
+    def PreBackup(self):
+        self.StopServer()
+
+
+    def PostBackup(self):
+        self.RestartServer()
+
+
+    def RestartServer(self):
+        print(f"####### Trying To Restart Server ############")
+        if self.IsServerPendingStart() or self.IsServerRunning():
+            print(f"Server Started or pending Start, not need to start again")
+            return
+        subprocess.run(self.windowsStartServiceCmd, check=True)   
+        self.SendEmailToAllUsers("Perforce Server Is Back Online", self.GetServerBackOnlineModeMsg())
+
+    def StopServer(self):
+        print(f"####### Trying To Stop Server ############")
+        if self.IsServerPendingStop() or self.IsServerStopped():
+            print(f"Server stopped or pending stop, no need to stop again")
+            return 
+        subprocess.run(self.windowsStopServiceCmd, check=True)
+        self.SendEmailToAllUsers("Perforce Server In Backup Mode", self.GetServerBackOnlineModeMsg())
+
+
+    def GetServeryStat(self):
+        try: 
+            result = subprocess.run(self.windowsQueryServiceCmd, capture_output=True, text=True)
+            print(f"---------------quried server stat: {result.stdout} \n -------------")
+            if self.runningState in result.stdout:
+                return self.runningState
+            
+            if self.startPendingState in result.stdout:
+                return self.startPendingState
+
+            if self.stopPendingState in result.stdout:
+                return self.stopPendingState
+
+        except subprocess.CalledProcessError as e:
+            print(f"Error checking server status: {e}")
+            return None
+
+
+class P4BackupLinux(P4Backup):
+    def __init__(self):
+        super().__init__()
+        print("Linux Backup Used")
+
+
+linuxBackupTest = P4BackupLinux()
