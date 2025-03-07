e1fe187ef5437cb221532e95caf5bc71efd8502b
Prof Li <jili1@uiwtx.edu>
2025-03-07 22:06:24 +0000

    trying to implement functions to interface with the system

--- src/ZFSUtils.py
+++ src/ZFSUtils.py
@@ -1,8 +1,47 @@
 import subprocess
 
 def GetZFSPoolAbsPath(zfsPoolName):
-    poolName = subprocess.run(["zfs", "get", "mountpoint", zfsPoolName])
+    processResult = subprocess.run(["zfs", "get", "mountpoint", zfsPoolName], capture_output=True, text=True, check=True)
+    
+    resultList = processResult.stdout.split("\n")
+    if len(resultList) > 1:
+        return resultList[1:]
+
+    return None
 
 
-poolPath = GetZFSPoolAbsPath("peforce_pool")
-print(poolPath)
+def ConvertSubprocessResultToDataSet(result):
+    resultLines = result.stdout.strip().split("\n")
+    if len(resultLines) <= 1:
+        return None
+
+    headers = RemoveEmptyEntry(resultLines[0].strip().split(" "))
+    
+    outDict = {}
+    for header in headers:
+        outDict[header] = []
+
+    for entryLine in resultLines[1:]:
+        entryValues = RemoveEmptyEntry(entryLine)
+        for i in range(0, len(entryValues)):
+
+
+
+
+    return headers
+
+
+def RemoveEmptyEntry(inList):
+    outList = []
+    for item in inList:
+        if item == "":
+            continue
+
+        outList.append(item)
+
+    return outList
+
+poolPath = GetZFSPoolAbsPath("perforce_pool")
+
+processResult = subprocess.run(["zfs", "get", "mountpoint", "perforce_pool"], capture_output=True, text=True, check=True)
+print(ConvertSubprocessResultToDataSet(processResult))
